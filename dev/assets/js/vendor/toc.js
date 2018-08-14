8c4aac685458f8aba950e119faf79ddf526b5d84
S.M. Rafiz <rafiz@codexin.com>
2018-08-14 14:07:11 +0600

    Initial Commit

---
+++ dev/assets/js/vendor/toc.js
@@ -0,0 +1,15 @@
+/******************************
+This file contains all vendor js
+
+INDEX:
+
+	v01 - jQuery UI
+	v02 - jQuery Nice Select
+	v03 - Swiper Slider
+	v04 - VenoBox
+	v05 - Photoswipe
+	v06 - simplyCountdown.js
+	v07 - Stellarnav Menu
+	v08 - Instafeed JS
+
+******************************/
